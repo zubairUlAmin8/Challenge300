f2eb37f5f13fd184a70758a976cc166e81de3622
Zubair Ul Amin <zubairulamin8@gmail.com>
2024-03-15 10:37:02 +0500

    C2_printIntString

---
+++ src/test/java/C1_ABCheck.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+
+/*AB Check
+
+Have the function ABCheck (str) take the stz parameter being passed and return
+the string true if the characters a and b are separated by exactly 3 places
+anywhere in the string at least once (ie. "lane borrowed" would result in true
+because there is exactly three characters between a and b). Otherwise return the string false.
+
+Examples
+Input: "after badly"
+Output: false
+
+Input: "Laura sobs"
+Output: true*/
+
+public class C1_ABCheck
+{
+}
---
+++ src/test/java/C2_printIntString.java
@@ -0,0 +1,44 @@
+import org.testng.annotations.Test;
+
+/*Given a string = “123AM256CD”, print two output strings,
+one for all the numbers and one for strings. Provide two ways. (Asked for the exact code)*/
+public class C2_printIntString {
+
+
+    @Test
+    public void printIntString(){
+        String str="123AM256CD";
+        String num="";
+        String character = "";
+
+        for(int i=0; i<str.length(); i++){
+            char temp=str.charAt(i);
+            if(temp>48 && temp<57){
+                System.out.println("Temp: "+ temp);
+                num+=temp;
+            } else if (temp>65 && temp<90) {
+                character+=temp;
+            }
+        }
+
+        System.out.println("Number String: "+ num);
+        System.out.println("Character String: "+ character);
+    }
+    @Test
+    public void printIntString2(){
+        String str="123AM256CD";
+        String num="";
+        String character = "";
+        for(int i=0; i<str.length(); i++){
+            char temp=str.charAt(i);
+            if(Character.isDigit(temp)){
+                num+=temp;
+            } else{
+                character+=temp;
+            }
+        }
+        System.out.println("Number String: "+ num);
+        System.out.println("Character String: "+ character);
+    }
+
+}
